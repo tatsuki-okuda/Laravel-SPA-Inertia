a4d7a57c7b75e00e44825f1fd8b6d6d4b3d9da22
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-07-07 19:17:11 +0900

    first

---
+++ resources/js/Pages/Another.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>{{ $page.props.appName}}</p>
+     <inertia-link :href="route('home')">Home</inertia-link>
+    this is Another
+  </div>
+</template>
---
+++ resources/js/Pages/Departments/Index.vue
@@ -0,0 +1,55 @@
+<template>
+    <breeze-authenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Departments
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        {{ departments }}
+                       <breeze-table>
+                          <template #header>
+                            <breeze-tc>Id</breeze-tc>
+                            <breeze-tc>Name</breeze-tc>
+                            <breeze-tc>Email</breeze-tc>
+                            <breeze-tc>Phone</breeze-tc>
+                            <breeze-tc>Actions</breeze-tc>
+                          </template>
+                           <tr v-for="d in departments" :key="d.id" class="hover:bg-gray-200">
+                              <breeze-tc>{{ d.id }}</breeze-tc>
+                              <breeze-tc>{{ d.name }}</breeze-tc>
+                              <breeze-tc>{{ d.email }}</breeze-tc>
+                              <breeze-tc>{{ d.phone }}</breeze-tc>
+                              <breeze-tc></breeze-tc>
+                            </tr>
+                       </breeze-table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </breeze-authenticated-layout>
+</template>
+
+<script>
+    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated'
+    import BreezeTc from '@/Components/TableColumn'
+    import BreezeTable from '@/Components/Table'
+
+    export default {
+        components: {
+            BreezeAuthenticatedLayout,
+            BreezeTc,
+            BreezeTable,
+        },
+        props: {
+          departments: Object,
+        },
+        setup(){
+
+        }
+    }
+</script>
